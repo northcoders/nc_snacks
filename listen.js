a8d3ac4961c8cfb91e13aca5b0c3ac5a20f03ccb
Rose Mullan <rfmullan@gmail.com>
2022-10-13 16:03:28 +0100

    hosting changes

--- listen.js
+++ listen.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
-app.listen(9090, () => {
-  console.log('app listening');
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
+  }
+);
