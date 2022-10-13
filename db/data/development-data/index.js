2f5b8e95f8acb77360c04352cae3a9f39d265b0e
Rose Mullan <rfmullan@gmail.com>
2022-10-13 15:29:52 +0100

    refactor into seed function

---
+++ db/data/development-data/index.js
@@ -0,0 +1,4 @@
+exports.snacksData = require('./snack-data.js');
+exports.drinksData = require('./drink-data.js');
+exports.categoriesData = require('./category-data.js');
+
---
+++ db/data/development-data/snack-data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        snack_name: 'Kit Kat'
+        , snack_description: 'time for a break'
+    },
+    {
+        snack_name: 'Hobnob',
+        snack_description: 'a modern classic'
+    },
+    {
+        snack_name: 'Banana',
+        snack_description: 'monkeys open them upside down'
+    },
+    {
+        snack_name: 'Chocolate Covered Raisins',
+        snack_description: 'you can never stop at one'
+    },
+    {
+        snack_name: 'Butter Croissant',
+        snack_description: 'always appreciated'
+    },
+    {
+        snack_name: 'Crisps',
+        snack_description: 'actually belong to Jim and he doesn\'t know i\'m stealing them, shhhhhh'
+    },
+]
