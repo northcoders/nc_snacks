48d4d34a84f1a693e84251a8901772fc0b82b4ce
Rose Mullan <rfmullan@gmail.com>
2023-11-22 10:21:28 +0000

    Update dev deat

--- db/data/development-data/category-data.js
+++ db/data/development-data/category-data.js
@@ -1,9 +1,9 @@
-module.exports = [
-    { category_name: 'Biscuit'},
-    { category_name: 'Fruit'},
-    { category_name: 'Chocolate'},
-    { category_name: 'Pastry'},
-    { category_name: 'Crisps'},
-    { category_name: 'Cake'},
-    { category_name: 'Other'}
+[
+    { "category_name": "Biscuit"},
+    { "category_name": "Fruit"},
+    { "category_name": "Chocolate"},
+    { "category_name": "Pastry"},
+    { "category_name": "Crisps"},
+    { "category_name": "Cake"},
+    { "category_name": "Other"}
 ]
--- db/data/development-data/drink-data.js
+++ db/data/development-data/drink-data.js
@@ -1,31 +1,32 @@
-module.exports = [
+[
     {
-        drink_name: 'Vimto',
-        drink_description: 'Manchester\'s finest'
+        "drink_name": "Vimto",
+        "drink_description": "Manchester's finest"
     },
     {
-        drink_name: 'Bovril',
-        drink_description: 'a meal that you don\'t have to chew'
+        "drink_name": "Bovril",
+        "drink_description": "a meal that you don't have to chew"
     },
     {
-        drink_name: 'Banana Milk',
-        drink_description: 'monkeys drink it upside down'
+        "drink_name": "Banana Milk",
+        "drink_description": "monkeys drink it upside down"
     },
     {
-        drink_name: 'Coffee',
-        drink_description: 'devs love Java'
+        "drink_name": "Coffee",
+        "drink_description": "devs love Java"
     },{
-        drink_name: '7up',
-        drink_description: 'if you want 6 cans of Sprite, make sure you don\'t pick 7up'
+        "drink_name": "7up",
+        "drink_description": "if you want 6 cans of Sprite, make sure you don\"t pick 7up"
     },{
-        drink_name: 'Tea',
-        drink_description: 'devs love PG tips'
+        "drink_name": "Tea",
+        "drink_description": "devs love PG tips"
     },
     {
-        drink_name: 'San Pellegrino',
-        drink_description: "someone's doing alright"
-    }, {
-        drink_name: 'PandaPop',
-        drink_description: 'hello, old friend'
+        "drink_name": "San Pellegrino",
+        "drink_description": "someone's doing alright"
+    },
+    {
+        "drink_name": "PandaPop",
+        "drink_description": "hello, old friend"
     }
   ]
