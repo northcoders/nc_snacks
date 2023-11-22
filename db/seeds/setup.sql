3648ea881b1550223f759c8560fb3f37d3b97534
Rose Mullan <rfmullan@gmail.com>
2023-11-22 10:58:34 +0000

    setup for end of create-server lecture

--- db/seeds/setup.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS nc_snacks_test;
-DROP DATABASE IF EXISTS nc_snacks;
-
-CREATE DATABASE nc_snacks_test;
-CREATE DATABASE nc_snacks;
---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((request, response) => {
+  const { url, method } = request;
+  console.log(`Received a ${method} request on ${url}`);
+
+  if (url === '/') {
+    response.setHeader('Content-Type', 'application/json');
+    response.statusCode = 200;
+    response.write(JSON.stringify({ msg: 'hello world' }));
+    response.end();
+  }
+
+  if (url === '/api/snacks') {
+    if (method === 'GET') {
+      fs.readFile('./data/snack-data.json', 'utf-8', (err, fileContents) => {
+        if (err) console.log(err);
+        else {
+          const snacks = JSON.parse(fileContents);
+          response.setHeader('Content-Type', 'application/json');
+          response.statusCode = 200;
+          response.write(JSON.stringify({ snacks }));
+          response.end();
+        }
+      });
+    }
+      if (method === 'POST') {
+          let body = ""
+          request.on('data', (data) => { 
+              body += data
+          })
+          request.on('end', () => { 
+              console.log(body)
+          })
+    } //Do not take the example any further, but encourage people to think about what would need to happen to the request body now.
+  }
+});
+
+server.listen(8080, (err) => {
+  if (err) console.log(err);
+  else console.log('Server listening on port: 8080');
+});
