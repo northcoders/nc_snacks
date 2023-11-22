e6d950a93b261fbee3e79cee5fff3b36179e76d4
Rose Mullan <rfmullan@gmail.com>
2023-11-22 10:19:56 +0000

    update test data

--- db/data/test-data/category-data.js
+++ db/data/test-data/category-data.js
@@ -1,6 +1,6 @@
-module.exports = [
-    { category_name: 'Category 1'},
-    { category_name: 'Category 4'},
-    { category_name: 'Category 3'},
-    { category_name: 'Category 2'},
+[
+    { "category_name": "Category 1"},
+    { "category_name": "Category 4"},
+    { "category_name": "Category 3"},
+    { "category_name": "Category 2"}
 ]
--- db/data/test-data/snack-data.js
+++ db/data/test-data/snack-data.js
@@ -1,22 +1,22 @@
-module.exports = [
+[
     {
-        snack_name: 'Snack 1',
-        snack_description: 'Snack description 1'
+        "snack_name": "Snack 1",
+        "snack_description": "Snack description 1"
     },
     {
-        snack_name: 'Snack 2',
-        snack_description: 'Snack description 2'
+        "snack_name": "Snack 2",
+        "snack_description": "Snack description 2"
     },
     {
-        snack_name: 'Snack 4',
-        snack_description: 'Snack description 4'
+        "snack_name": "Snack 4",
+        "snack_description": "Snack description 4"
     },
     {
-        snack_name: 'Snack 5',
-        snack_description: 'Snack description 6'
+        "snack_name": "Snack 5",
+        "snack_description": "Snack description 6"
     },
     {
-        snack_name: 'Snack 3',
-        snack_description: 'Snack description 3'
-    },
+        "snack_name": "Snack 3",
+        "snack_description": "Snack description 3"
+    }
 ]
