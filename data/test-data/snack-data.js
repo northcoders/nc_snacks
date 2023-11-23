8751005a72d17c7e8b99d531073af564b4693480
Rose Mullan <rfmullan@gmail.com>
2023-11-23 15:13:58 +0000

    resolve merge error

---
+++ data/test-data/snack-data.js
@@ -0,0 +1,24 @@
+[
+    {   "snack_id": 1,
+        "snack_name": "Snack A",
+        "snack_description": "Snack description B"
+    },
+    {   "snack_id": 2,
+        "snack_name": "Snack B",
+        "snack_description": "Snack description B"
+    },
+    {   
+        "snack_id": 4,
+        "snack_name": "Snack D",
+        "snack_description": "Snack description D"
+    },
+    {   "snack_id": 5,
+        "snack_name": "Snack E",
+        "snack_description": "Snack description E"
+    },
+    {
+        "snack_id": 6,
+        "snack_name": "Snack F",
+        "snack_description": "Snack description F"
+    }
+]
