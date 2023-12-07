16428187e2357a15d121c63e61a4b28b50f11998
Rose Mullan <rfmullan@gmail.com>
2023-12-07 11:11:27 +0000

    update example to include lecture progress

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+const pool = new Pool();
+
+function getSnacks() {
+    pool.query('SELECT * FROM snacks;').then(({rows: snacks}) => {
+        console.log(snacks);
+        pool.end()
+    });
+}
+
+// getSnacks()
+
+function getSnackById(id) { 
+    pool.query(`SELECT * FROM snacks WHERE snack_id=${id}`).then(({ rows: [snack] }) => {
+        console.log(snack)
+        pool.end()
+    })
+
+}
+
+// getSnackById(3)
