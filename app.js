c6dfaee0b339b4035b3112b5561c8d95258a66d5
Rose Mullan <rfmullan@gmail.com>
2023-12-11 11:59:47 +0000

    resolving merge error

--- app.js
+++ app.js
@@ -16,21 +16,7 @@ app.get('/api/snacks', (request, response) => {
     })
 })
 
-<<<<<<< HEAD
-app.get('/api/snacks/:snack_id', (request, response) => {
-  const { snack_id } = request.params;
-  fs.readFile('./data/snack-data.json', 'utf-8')
-    .then((fileContents) => {
-      const snacks = JSON.parse(fileContents);
-      const singleSnack = snacks.filter((snack) => {
-        return snack.snack_id === +snack_id;
-      })[0];
-      response.status(200).send({ snack: singleSnack });
-    });
-})
-=======
 app.get('/api/snacks/:snack_id', getSnackBySnackId)
->>>>>>> 2ad29186cce9e41540dc4fdd256d1f27aa7612dc
 
 app.post('/api/snacks', (request, response) => {
   const newSnack = request.body;
