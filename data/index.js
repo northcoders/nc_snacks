e1c42d1ff3d1db6a4f21aab0f343a7996fbf02bd
Rose Mullan <rfmullan@gmail.com>
2023-12-12 14:43:06 +0000

    data malarkey

--- data/index.js
+++ data/index.js
@@ -1,4 +1,5 @@
 exports.snacksData = require('./snack-data');
 exports.drinksData = require('./drink-data')
 exports.categoriesData = require('./category-data');
+exports.vendingMachineData = require('./vending-machine-data')
 
