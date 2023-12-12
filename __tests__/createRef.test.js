cbc46ccb1165489c815247514dc792ed8ce81cb9
Rose Mullan <rfmullan@gmail.com>
2023-12-12 14:16:48 +0000

    update for supertest WIP

---
+++ __tests__/createRef.test.js
@@ -0,0 +1,7 @@
+const createRef = require('../utils.js');
+
+describe("createRef", () => { 
+    it("returns the relevant key-value pair when passed a single-item array", () => { 
+        expect(createRef([]))
+    })
+})
